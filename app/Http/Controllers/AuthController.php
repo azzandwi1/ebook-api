fcce0e82757ecbbc96f31a394256b2579b534e7c
azzandwi01 <azzandwiriski8@gmail.com>
2020-08-09 13:26:24 +0700

    endpoint

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function me(){
+        return "NIS : 3103118031
+                NAMA : AZZAN DWI RISKI
+                GENDER : LAKI - LAKI
+                PHONE : 085866073348
+                KELAS : XI RPL 1";
+    }
+}
